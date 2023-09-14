772e4f0189e6379996e076efca9f2a361fe12c94
jinho-choi123 <choijinho817@gmail.com>
2023-09-14 03:40:30 +0000

    fix: add dotenv to gitignore

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import { connectDB } from "./db";
 
 const app = express();
 
+const SERVER_PORT = parseInt(process.env.SERVER_PORT) || 8888;
+
 //load env
 loadEnv();
 
 //connect to mongo db 
 connectDB();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/hc", (req, res) => {
+    res.send("Server is Running!");
 })
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Server is running on port ${Bun.env.SERVER_PORT}`)
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on port ${process.env.SERVER_PORT}`)
 })
